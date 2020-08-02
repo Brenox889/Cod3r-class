168f1ed0fc89b2f926dc759a285cd8e7a520fb7f
Breno Mendes <brenomendes889@hotmail.com>
2020-08-02 16:41:38 -0300

    feat: callbackToPromises

--- esnext/promises.js
+++ esnext/promises.js
@@ -9,3 +9,4 @@ function falarDepoisDe(segundos, frase){
 falarDepoisDe(3 ,'Que legal')
     .then(frase => frase.concat('?!?'))
     .then(outraFrase => console.log(outraFrase))
+    .catch(e=>console.log(e))
