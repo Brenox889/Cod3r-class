3c40ec22fefd9a0def690266beeb6246abc6249e
Breno Mendes <brenomendes889@hotmail.com>
2020-08-02 17:46:17 -0300

    feat: async/await

---
+++ esnext/async-await.js
@@ -0,0 +1,26 @@
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject)=>{
+        http.get(url, res =>{
+            let resultado = ''
+
+            res.on('data', dados =>{
+                resultado += dados
+            })
+            res.on('end',()=>{
+                try{
+                callback(JSON.parse(resultado))
+                }catch(e){
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+let obterAlunos = async() =>{
+    const ta = await getTurma('A')
+    const tb = await getTurma('B')
+    const tc = await getTurma('C')
+    return [].concat(ta, tb, tc)
+}
